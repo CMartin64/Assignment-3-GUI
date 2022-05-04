fd9b87eabc7c56fc5c26e1ae5696ee75ff5a8858
Callum-F <callumfreeburn1996@gmail.com>
2022-05-04 15:31:55 +1000

    added database and view page

--- GUI_Flask_App.py
+++ GUI_Flask_App.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 print('got here')
 # def create_app():
 
@@ -10,6 +12,33 @@ bookDetails['Genre'] = 'Biographical'
 
 
 app = Flask(__name__)
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.sqlite3'
+db = SQLAlchemy(app)
+
+class Books(db.Model):
+   id = db.Column('book_id', db.Integer, primary_key = True)
+   Title = db.Column(db.String(100))
+   Author = db.Column(db.String(100))  
+   Genre = db.Column(db.String(100))
+   Publisher = db.Column(db.String(10))
+   
+   def __init__(self, Title, Author, Genre,Publisher):
+    self.Title = Title
+    self.Author = Author
+    self.Genre = Genre
+    self.Publisher = Publisher
+
+db.create_all()
+
+entry = Books('Life of Student', 'Callum Freeburn', 'Fiction', 'self-publisher')
+db.session.add(entry)
+db.session.commit()
+
+users = Books.query.all()
+
+# for user in users:
+#     print(user.Title)
+
 
 @app.route('/')
 def index():
@@ -27,6 +56,10 @@ def upload_file():
 @app.route('/bookDetails')
 def book_details():
     return render_template('book_details.html', bookDetails=bookDetails, image = 'static/pic_trulli.jpg')
+
+@app.route('/show_all')
+def show_all():
+   return render_template('show_all.html', Books = Books.query.all() )
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ booksDatabase.py
@@ -0,0 +1,16 @@
+# from flask_sqlalchemy import SQLAlchemy
+
+
+# db = SQLAlchemy(app)
+# class books(db.Model):
+#    id = db.Column('book_id', db.Integer, primary_key = True)
+#    Title = db.Column(db.String(100))
+#    Author = db.Column(db.String(100))  
+#    Genre = db.Column(db.String(100))
+#    Publisher = db.Column(db.String(10))
+   
+#    def __init__(self, Title, Author, Genre,Publisher):
+#     self.Title = Title
+#     self.Author = Author
+#     self.Genre = Genre
+#     self.Publisher = Publisher
