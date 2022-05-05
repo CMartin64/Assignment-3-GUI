f29dea8e13c73c7492302a10adbbb9d1ee61f55d
Callum-F <callumfreeburn1996@gmail.com>
2022-05-05 20:28:40 +1000

    added formatting

--- GUI_Flask_App.py
+++ GUI_Flask_App.py
@@ -1,18 +1,22 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-
+from werkzeug.utils import secure_filename
+import os
 print('got here')
-# def create_app():
 
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+UPLOAD_FOLDER = './'
 
 bookDetails = {}
 bookDetails['Title'] = 'Mr Smithsonian'
 bookDetails['Author'] = 'Mrs Smithsonian'
 bookDetails['Genre'] = 'Biographical'
-
+bookDetails['Publisher'] = 'Self-Published'
 
 app = Flask(__name__)
 app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.sqlite3'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] = 'the random string'    
 db = SQLAlchemy(app)
 
 class Books(db.Model):
@@ -39,6 +43,7 @@ users = Books.query.all()
 # for user in users:
 #     print(user.Title)
 
+def allowed_file(filename):     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def index():
@@ -46,11 +51,21 @@ def index():
 
 @app.route('/', methods=['POST', 'GET'])
 def upload_file():
-    
-        uploaded_file = request.files['file']
-        if uploaded_file.filename != '':
-            uploaded_file.save(uploaded_file.filename)
-        return redirect(url_for('index'))
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return redirect(url_for('index'))
 
 
 @app.route('/bookDetails')
