4873702d18b96e32a7023af1682e7e4272bf054a
Callum-F <callumfreeburn1996@gmail.com>
2022-05-06 15:02:19 +1000

    added style to nav bar

--- GUI_Flask_App.py
+++ GUI_Flask_App.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
+from flask_navigation import Navigation
 import os
 print('got here')
 
@@ -18,7 +19,12 @@ app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.sqlite3'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = 'the random string'    
 db = SQLAlchemy(app)
+nav = Navigation(app)
 
+nav.Bar('top', [
+    nav.Item('Upload Book Cover', 'upload_cover'),
+    nav.Item('Show Books Database', 'show_database', {'page': 1}),
+])
 class Books(db.Model):
    id = db.Column('book_id', db.Integer, primary_key = True)
    Title = db.Column(db.String(100))
@@ -40,32 +46,27 @@ db.session.commit()
 
 users = Books.query.all()
 
-# for user in users:
-#     print(user.Title)
-
 def allowed_file(filename):     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def upload_cover():
+    return render_template('uploadCover.html')
 
 @app.route('/', methods=['POST', 'GET'])
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    return redirect(url_for('index'))
+            print(file)
+    return redirect(url_for('upload_cover'))
 
 
 @app.route('/bookDetails')
@@ -73,7 +74,7 @@ def book_details():
     return render_template('book_details.html', bookDetails=bookDetails, image = 'static/pic_trulli.jpg')
 
 @app.route('/show_all')
-def show_all():
+def show_database():
    return render_template('show_all.html', Books = Books.query.all() )
 
 if __name__ == '__main__':
