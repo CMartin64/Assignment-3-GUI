449061cf7ca1e91c6811f0857d951bfbda045d29
Patrick Espiritu <Patrick.L.Espiritu@gmail.com>
2022-05-26 21:02:35 +1000

    Genre model initial upload

---
+++ genre_model.py
@@ -0,0 +1,56 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+
+
+class GenreModel:
+    class_names = ['Arts & Photography', 'Biographies & Memoirs', 'Business & Money', 'Calendars',
+                   "Children's Books",
+                   'Comics & Graphic Novels', 'Computers & Technology', 'Cookbooks, Food & Wine',
+                   'Crafts, Hobbies & Home', 'Christian Books & Bibles', 'Engineering & Transportation',
+                   'Health, Fitness & Dieting', 'History', 'Humor & Entertainment', 'Law', 'Literature & Fiction',
+                   'Medical Books', 'Mystery, Thriller & Suspense', 'Parenting & Relationships',
+                   'Politics & Social Sciences', 'Reference', 'Religion & Spirituality', 'Romance',
+                   'Science & Math',
+                   'Science Fiction & Fantasy', 'Self-Help', 'Sports & Outdoors', 'Teen & Young Adult',
+                   'Test Preparation', 'Travel']
+
+    def __init__(self, model_path='genreModel.h5'):
+        self.genre_model = tf.keras.models.load_model(model_path)
+
+    def predict_genre(self, img_path):
+        """
+
+        Args:
+            img_path: path of image to make prediction on
+
+        Returns:
+            Genre prediction and confidence/probability
+
+        """
+
+        # Load and preprocess image
+        img = image.load_img(img_path, target_size=(224, 224))
+
+        # plt.imshow(img)
+        # plt.show()
+
+        img = image.img_to_array(img)
+        img = np.expand_dims(img, axis=0)
+        img = img / 255.
+
+        # Predict genre
+        predictions = self.genre_model.predict(img)
+        genre_prediction = self.class_names[np.argmax(predictions)]
+
+        # Get probability/confidence
+        prob = np.max(predictions, axis=1)[0]
+
+        return genre_prediction, prob
+
+
+if __name__ == '__main__':
+    model = GenreModel()
+
+    genre, confidence = model.predict_genre('Images/why_nations_fail.jpg')
+    print(f'Genre: {genre} --- Confidence: {confidence}')
